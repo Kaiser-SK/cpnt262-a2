4ada6f3803b772b2bb566b84f90595122874fa39
Sebastian Kaiser <sebastian.kaiser@edu.sait.ca>
2022-10-27 23:02:39 -0600

    majority of functionality added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cpnt262-a2
+
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const defaultMode = document.getElementById('default-theme');
+const darkMode = document.getElementById('dark-theme');
+const halloweenMode = document.getElementById('halloween-theme');
+const christmasMode = document.getElementById('christmas-theme');
+const themeTitle = document.querySelector('.theme-selection')
+
+const body = document.querySelector('body');
+
+const changeTheme = function() {
+  if (defaultMode.checked) {
+    body.classList.replace('default','default');
+    themeTitle.innerHTML = "Default Theme";
+  } 
+  else if (halloweenMode.checked) {
+    body.classList.replace('default','halloween');
+    themeTitle.innerHTML = "Halloween Theme";
+  } 
+  else if (christmasMode.checked) {
+    body.classList.replace('default','christmas');
+    themeTitle.innerHTML = "Christmas Theme";
+  } 
+  else if (darkMode.checked) {
+    body.classList.replace('default','dark');
+    themeTitle.innerHTML = "Dark Mode";
+  } 
+  // else {
+    //   body.classList.replace('dark','light');
+  //   themeTitle.innerHTML = "Dark Mode";
+  // }
+}
+
+defaultMode.addEventListener('click', changeTheme);
+darkMode.addEventListener('click', changeTheme);
+halloweenMode.addEventListener('click', changeTheme);
+christmasMode.addEventListener('click', changeTheme);
